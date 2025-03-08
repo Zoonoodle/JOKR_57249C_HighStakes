7c10ed5e635010be9d74a4877a1865528a5cb53c
Zoonoodle <56184795+Zoonoodle@users.noreply.github.com>
2025-03-08 11:18:35 -0800

    Auto-update: 2025-03-08 11:18:35

--- src/autons/rightSideMidRush.cpp
+++ src/autons/rightSideMidRush.cpp
@@ -56,7 +56,7 @@ chassis.turnToHeading(-325, 400);
     chassis.turnToHeading(115, 450);
 
     //grab first middle
-    chassis.moveToPoint(1, -38, 900);
+    chassis.moveToPoint(-1, -38, 900);
     doinker.set_value(true);
     
 
@@ -97,9 +97,10 @@ chassis.turnToHeading(-325, 400);
   
 //    intakeLift.set_value(false);
 //    pros::delay(100);
- chassis.moveToPoint(-51,6, 1200, {.minSpeed = 90});
- chassis.moveToPoint(-49, 3.5, 700, {.forwards = false, .maxSpeed = 60, .minSpeed =60, .earlyExitRange = 1});
- chassis.moveToPoint(-51,6, 900, {.minSpeed = 90});
+ chassis.moveToPoint(-51,7, 1300, {.minSpeed = 90});
+ chassis.moveToPoint(-49, 2, 700, {.forwards = false, .maxSpeed = 60, .minSpeed =40});
+ chassis.waitUntilDone();
+ chassis.moveToPoint(-51,7, 900, {.minSpeed = 90});
 intakeLift.set_value(true);
     pros::delay(400);
     intakeLift.set_value(false);
@@ -110,11 +111,11 @@ intakeLift.set_value(true);
     setAutonState(1);
     chassis.turnToHeading(140, 500, {.earlyExitRange = 10});
     currentIntakeCommand = STOPRED;
-    chassis.moveToPoint(-43, -40, 1000);
+    chassis.moveToPoint(-43, -38, 1000);
     intake.move(127);
     mogo.set_value(false);
 
-    chassis.turnToHeading(235, 650);
+    chassis.turnToHeading(240, 650);
     currentArmState = 1; 
     loadActivated   = true; 
     targetArmState  = LoadStates[1]; 
